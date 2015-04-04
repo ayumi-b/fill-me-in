be0916d6cdff94ff7baca70cffbcea5b55877a0a
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-04 12:48:31 -0500

    able to pull one user info into table

--- js/journal/show.controller.js
+++ js/journal/show.controller.js
@@ -2,25 +2,33 @@ angular
   .module('kids')
   .controller('ShowController', ShowController);
 
-function ShowController( $http, journalFactory, $scope) {
+function ShowController( $http, journalFactory, $scope, BASE_URL) {
   var vm = this;
-  vm.journals = {};
+  // vm.journals = {};
   vm.journal;
+  vm.journalUsers;
 
-  journalFactory.findAll(function (journals) {
-    var journalVals = [];
-    var Arrays = [];
-    var vals = Object.keys(journals).map(function(key){
-      return journals[key];
+  $http.get(BASE_URL + "/users/simplelogin%3A35/journal.json")
+  .success(
+    function(data){
+      vm.journalUsers = data;
+      console.log(vm.journalUsers);
     });
-    for(var i = 0; i<vals.length; i++){
-      Arrays.push(Object.keys(vals[i].journal).map(function(key){
-      return vals[i].journal[key];
-      }));
-    }
-    journalVals = journalVals.concat.apply(journalVals, Arrays);
-    $scope.journalVals = journalVals;
-  });
+
+  // journalFactory.findAll(function (journals) {
+  //   var journalVals = [];
+  //   var Arrays = [];
+  //   var vals = Object.keys(journals).map(function(key){
+  //     return journals[key];
+  //   });
+  //   for(var i = 0; i<vals.length; i++){
+  //     Arrays.push(Object.keys(vals[i].journal).map(function(key){
+  //     return vals[i].journal[key];
+  //     }));
+  //   }
+  //   journalVals = journalVals.concat.apply(journalVals, Arrays);
+  //   $scope.journalVals = journalVals;
+  // });
     //   vm.journalPop(function (journals) {
 };
     // });
